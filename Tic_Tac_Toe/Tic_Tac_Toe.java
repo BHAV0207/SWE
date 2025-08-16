d31841c2ddd2b2f57d2c4a388fd322279952f4fc
BHAV0207 <bhavyajain4274@gmail.com>
2025-08-16 14:42:38 +0530

    made and displayed the grid

--- Tic_Tac_Toe/Tic_Tac_Toe.java
+++ Tic_Tac_Toe/Tic_Tac_Toe.java
@@ -1,18 +1,35 @@
 class Tic_Tac_Toe {
   public static void main(String[] args) {
-    Game game = new Game(5);
-    game.start();
+    Game game = new Game(3);
+    game.displayBoard();
   }
 }
 
 class Game {
   private int size;
+  private char board[][];
 
   Game(int size) {
     this.size = size;
+    board = new char[size][size];
+    initialiseBoard();
   }
 
-  void start() {
-    System.out.println("Game created with size" + " " + size);
+  private void initialiseBoard() {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        board[i][j] = '-';
+      }
+    }
   }
+
+  public void displayBoard() {
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        System.out.print(board[row][col] + " ");
+      }
+      System.out.println();
+    }
+  }
+
 }
