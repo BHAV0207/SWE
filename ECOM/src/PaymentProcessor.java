0e5b586723e6044f6ae89f613f58c1824ce3117e
BHAV0207 <bhavyajain4274@gmail.com>
2025-08-24 22:21:26 +0530

    done with the first assignment

--- ECOM/src/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-public class PaymentProcessor {
-  void pay(int productID) {
-    System.out.println();
-    SqlProductRepo spr = new SqlProductRepo();
-    spr.getProductById(productID);
-    System.out.println("Processing payment for this product...");
-    System.out.println();
-  }
-}
