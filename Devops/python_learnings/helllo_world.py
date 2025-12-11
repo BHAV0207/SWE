50e1daa0a66824c907b931c24514899a6e176406
BHAV0207 <bhavyajain4274@gmail.com>
2025-12-11 13:11:35 +0530

    added the notes of basic python for devops

---
+++ Devops/python_learnings/helllo_world.py
@@ -0,0 +1,1 @@
+print("hello world ,bhvaya jain this side")
---
+++ Devops/python_learnings/objects.py
@@ -0,0 +1,22 @@
+a =[1,2,3,4,5,6,7,8,9,10]
+b = ["bhavya","jain","is","a","good","girl"]
+print(a)
+
+sliced_a = a[2:8]
+print(sliced_a)
+
+a.append(b)
+print(a)
+
+tuple_a = (1,2,3,4,5,6,7,8,9,10)
+print(tuple_a)
+
+
+set_a = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5}
+print(set_a)
+
+dict_a = {"name":"bhavya",
+          "age":20,
+          "is_student":True}
+print(dict_a) 
+
